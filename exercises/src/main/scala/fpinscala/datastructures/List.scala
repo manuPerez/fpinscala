cfd0e85d33bc09cebd4a9165ac9e5fd6f7a046c3
Manu <perezdelgadomanu@gmail.com>
2016-11-13 12:21:19 +0100

    Exercises 3.6, 3.9, 3.10, 3.11, 3.12 and 3.14

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -66,15 +66,42 @@ object List { // `List` companion object. Contains functions for creating and wo
   }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case(h, t) if(f(h)) => dropWhile(t, f)
+    case Cons(h, t) if(f(h)) => dropWhile(t, f)
     case _ => l
   }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("l sis empty")
+    case Cons(h, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, acum) => acum + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    l match {
+        case Nil => z
+        case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+      }
+  }
+
+  def sum3(l: List[Int]): Int =
+    foldLeft(l, 0)((x,y) => x + y)
+
+  def product3(l: List[Double]): Double =
+    foldLeft(l, 1.0)(_*_)
+
+  def length3[A](l: List[A]): Int =
+    foldLeft(l, 0)((acum, _) => acum + 1)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((li, h) => Cons(h, li))
+
+  def append2[A](a1: List[A], a2: List[A]): List[A] =
+    foldRight(a1, foldRight(a2, List[A]())((h, li) => Cons(h, li)))((h2, li2) => Cons(h2, li2))
+
 
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
@@ -98,5 +125,22 @@ object TestList {
 
     //Exercise 3.4
     println(List.drop(List(1,2,3,4,5), 2))
+
+    //Exercise 3.6
+    println(List.init(List(1,2,3,4,5)))
+
+    //Exercise 3.9
+    println(List.length(List(10,12,13,14,15)))
+
+    //Exercise 3.11
+    println(List.sum3(List(1,2,3,4,5)))
+    println(List.product3(List(1,2,3,4,5)))
+    println(List.length3(List(10,12,13,14,15)))
+
+    //Exercise 3.12
+    println(List.reverse(List(1,2,3)))
+
+    //Exercise 3.14
+    println(List.append2(List(1,2), List(3,4)))
   }
 }
